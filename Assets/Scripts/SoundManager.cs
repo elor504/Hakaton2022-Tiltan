01c3f2eb39b829f365749dcc15eb7a48b85a68b2
JinMoshchenko <jinmoshchenko@gmail.com>
2022-08-05 15:27:02 +0300

    ds

--- Assets/Scripts/SoundManager.cs
+++ Assets/Scripts/SoundManager.cs
@@ -19,6 +19,8 @@ public class SoundManager : MonoBehaviour
         {
             Destroy(this.gameObject);
         }
+
+        PlayGameplayMusic(true);
     }
 
     public void PlayGameplayMusic(bool isPlay)
--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -1,18 +1,61 @@
-using System.Collections;
-using System.Collections.Generic;
+using UnityEngine.UI;
 using UnityEngine;
 
 public class UIManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField]GameObject settingsMenu;
+    [SerializeField] Image soundsMuteImg, musicMuteImg;
+    bool isOpen;
+    bool muteSfx, muteMusic;
+
+    public void OpenSettingsMenu()
     {
-        
+        if(isOpen == false)
+        {
+            settingsMenu.SetActive(true);
+            isOpen = true;
+        }
+        else
+        {
+            settingsMenu.SetActive(false);
+            isOpen = false; 
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    public void ChangeSounds()
     {
-        
+        if (muteSfx == false)
+        {
+            soundsMuteImg.enabled = true;
+            muteSfx = true;
+            for (int i = 0; i < SoundManager.getInstance.musicList.Length; i++)
+            {
+                SoundManager.getInstance.sfxList[i].volume = 0;
+            }
+        }
+        else
+        {
+            soundsMuteImg.enabled = false;
+            muteSfx = false;
+            for (int i = 0; i < SoundManager.getInstance.musicList.Length; i++)
+            {
+                SoundManager.getInstance.sfxList[i].volume = 0.3f;
+            }
+        }
+    }
+    public void ChangeMusic()
+    {
+        if (muteMusic == false)
+        {
+            musicMuteImg.enabled = true;
+            muteMusic = true;
+            SoundManager.getInstance.musicList[0].volume = 0;
+        }
+        else
+        {
+            musicMuteImg.enabled = false;
+            muteMusic = false;
+            SoundManager.getInstance.musicList[0].volume = 0.1f;
+        }
     }
 }
