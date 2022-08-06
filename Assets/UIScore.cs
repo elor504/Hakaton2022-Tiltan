f444d084af58e25a97cd8b833ca850d859f132fe
elor504 <74613768+elor504@users.noreply.github.com>
2022-08-06 14:20:59 +0300

    rounded text

--- Assets/UIScore.cs
+++ Assets/UIScore.cs
@@ -31,14 +31,32 @@ public class UIScore : MonoBehaviour
 		else if (scoreAmount >= 1000 && scoreAmount < 1000000)
 		{
 			type = "K";
-			amount = Mathf.FloorToInt(scoreAmount) * 0.001f;
-			score.text = amount.ToString("F1") + type;
+			amount = Mathf.Round(scoreAmount) * 0.001f;
+
+			if (Mathf.Approximately(amount, Mathf.RoundToInt(amount)))
+			{
+				score.text = amount.ToString() + type;
+			}
+			else
+			{
+				score.text = amount.ToString("F2") + type;
+			}
 		}
 		else if (scoreAmount >= 1000000)
 		{
 			type = "M";
 			amount = Mathf.Round((scoreAmount / 1000000) * 10.0f) * 0.1f;
-			score.text = amount.ToString("F1") + type;
+
+			if (Mathf.Approximately(amount, Mathf.RoundToInt(amount)))
+			{
+				score.text = amount.ToString() + type;
+			}
+			else
+			{
+				score.text = amount.ToString("F2") + type;
+			}
+
+			//score.text = amount.ToString("F1") + type;
 		}
 
 
